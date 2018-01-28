1adc0cf574661664694c29bd3425fe50663a8df8
Ryan Sprott <rdsprott@gmail.com>
2018-01-28 15:05:45 -0500

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get '/get_buses_for_stop/:id' => 'api#get_buses_for_stop', as: 'get_buses_for_stop'
+  get '/get_stop_info/:id' => 'api#get_stop_info', as: 'get_stop_info'
+  get '/get_stops_for_route/:id' => 'api#get_stops_for_route', as: 'get_stops_for_route'
+  get '/get_nearby_stops' => 'api#get_nearby_stops', as: 'get_nearby_stops'
+  get '/get_schedule_for_stop/:id' => 'api#get_schedule_for_stop', as: 'get_schedule_for_stop' 
 end
